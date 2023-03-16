0189bfff46c0d65e9eb37bd3a2eb8d38f048b436
TalHIT <109478316+TalHIT@users.noreply.github.com>
2023-03-16 12:13:43 +0200

    Moving the logic to background

--- src/background.js
+++ src/background.js
@@ -5,45 +5,58 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-// async () => {
-//   await chrome.runtime.onMessage.addListener(
-//     (request, sender, sendResponse) => {
-//       // if (request.type === 'GREETINGS') {
-//       //   const message = `Hi ${
-//       //     sender.tab ? 'Con' : 'Pop'
-//       //   }, my name is Bac. I am from Background. It's great to hear from you.`;
+function countDown(distance) {
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-//       //   // Log message coming from the `request` parameter
-//       //   console.log(request.payload.message);
-//       //   // Send a response message
-//       //   sendResponse({
-//       //     message,
-//       //   });
-//       // }
+  let hebDays = 'ימים';
+  let hebHours = 'שעות';
+  let hebMin = 'דקות';
 
-//       const urlArray = request.timeString;
-//       timeString.forEach(function (taskPage) {
-//         const tableHtml = Array.from(taskPage.getElementsByTagName('tbody'));
-//         let dateString = String(tableHtml[0].innerHTML);
-//         dateString = dateString.match(
-//           /<th class="cell c0" style="" scope="row">עד לתאריך<\/th>.*?<\/td>/gms
-//         );
-//         dateString = dateString[0].replace(/<.*>\n/, '');
-//         dateString = dateString.replace(/<.*?>/gm, '');
-//         const date_time = dateString.split(', ');
-//         const date = date_time[0].split('/');
-//         const time = date_time[1].split(':');
-//         const date_1 = new Date(
-//           date[2],
-//           date[1],
-//           date[0],
-//           time[0],
-//           time[1]
-//         ).getTime();
-//         sendResponse(date_1);
-//       });
-// console.log('TIMEARRAY: ', timeArray);
-// sendResponse(timeArray);
-//     }
-//   );
-// };
+  return `${days > 0 ? hebDays + ' : ' : ''}${days > 0 ? days + ' | ' : ''}${
+    hours > 0 ? hebHours + ' : ' : ''
+  }${hours > 0 ? hours + ' | ' : ''}${minutes > 0 ? hebMin + ' : ' : ''}${
+    minutes > 0 ? minutes : ''
+  } `;
+}
+
+function cleanHtml(html) {
+  let dateString = html.match(
+    /<th class="cell c0" style="" scope="row">עד לתאריך<\/th>.*?<\/td>/gms
+  );
+  dateString = dateString[0].replace(/<.*>\n/, '');
+  dateString = dateString.replace(/<.*?>/gm, '');
+  const date_time = dateString.split(', ');
+  const date = date_time[0].split('/');
+  const time = date_time[1].split(':');
+  return new Date(
+    date[2],
+    Number(date[1]) - 1,
+    date[0],
+    time[0],
+    time[1]
+  ).getTime();
+}
+
+function isSubmitted(html) {
+  let dateString = html.match(
+    /<th class="cell c0" style="" scope="row">מצב ההגשה<\/th>.*?<\/td>/gms
+  );
+  dateString = dateString[0].replace(/<.*>\n/, '');
+  dateString = dateString.replace(/<.*?>/gm, '');
+  return dateString.includes('אין');
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request) {
+    if (request.att === 'countDown') {
+      const milisec = cleanHtml(request.html);
+      let distance = milisec - new Date().getTime();
+      const countString = countDown(distance);
+      sendResponse(countString);
+    if (request.att === 'isSubmitted') {
+      sendResponse(isSubmitted(request.html));
+    }
+  }
+});
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function getTopElements() {
   const higherPart = Array.from(
     document.getElementsByClassName('dropdown')
@@ -10,52 +11,8 @@ function getTopElements() {
   return changeTime.filter((task) => !task.classList.contains('disabled'));
 }
 
-function cleanHtml(html) {
-  let dateString = html.match(
-    /<th class="cell c0" style="" scope="row">עד לתאריך<\/th>.*?<\/td>/gms
-  );
-  dateString = dateString[0].replace(/<.*>\n/, '');
-  dateString = dateString.replace(/<.*?>/gm, '');
-  const date_time = dateString.split(', ');
-  const date = date_time[0].split('/');
-  const time = date_time[1].split(':');
-  return new Date(
-    date[2],
-    Number(date[1]) - 1,
-    date[0],
-    time[0],
-    time[1]
-  ).getTime();
-}
-
-function countDown(distance) {
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-
-  let hebDays = 'ימים';
-  let hebHours = 'שעות';
-  let hebMin = 'דקות';
-
-  return `${days > 0 ? hebDays + ' : ' : ''}${days > 0 ? days + ' | ' : ''}${
-    hours > 0 ? hebHours + ' : ' : ''
-  }${hours > 0 ? hours + ' | ' : ''}${minutes > 0 ? hebMin + ' : ' : ''}${
-    minutes > 0 ? minutes : ''
-  } `;
-}
-
-function isSubmitted(html) {
-  let dateString = html.match(
-    /<th class="cell c0" style="" scope="row">מצב ההגשה<\/th>.*?<\/td>/gms
-  );
-  dateString = dateString[0].replace(/<.*>\n/, '');
-  dateString = dateString.replace(/<.*?>/gm, '');
-  return dateString.includes('אין');
-}
-
 async function getTasksTimes() {
   const tasks = getTopElements();
-  const timeString = [];
   tasks.forEach(async (task) => {
     const url = task.getElementsByTagName('a')[0]['href'];
     const xhr = new XMLHttpRequest();
@@ -67,13 +24,20 @@ async function getTasksTimes() {
         const taskPage = await xhr.response;
         const tableHtml = Array.from(taskPage.getElementsByTagName('tbody'));
         const html = String(tableHtml[0].innerHTML);
-        if (isSubmitted(html)) {
-          const milisec = cleanHtml(html);
-          let distance = milisec - new Date().getTime();
+        const submitted = await chrome.runtime.sendMessage({
+          html: html,
+          att: 'isSubmitted',
+        });
+        console.log(submitted);
+        if (submitted) {
+          const countDown = await chrome.runtime.sendMessage({
+            html: html,
+            att: 'countDown',
+          });
+          console.log(countDown);
           let spanText = task.getElementsByTagName('span');
-          const count_down = countDown(distance);
           spanText[0].innerHTML =
-            count_down === '' ? 'זמן ההגשה עבר' : count_down;
+            countDown === '' ? 'זמן ההגשה עבר' : countDown;
           if (distance < 86400000) {
             // if the date is smaller than 1 day
 
@@ -93,100 +57,6 @@ async function getTasksTimes() {
       }
     };
   });
-
-  return timeString;
 }
 
 getTasksTimes();
-
-// async function createMilisecTimes() {
-//   const timeString = await getTasksTimes(tasks);
-//   console.log(timeString);
-//   const timeLeft = [];
-//   timeString.forEach((taskPage) => {
-//     dateString = dateString.match(
-//       /<th class="cell c0" style="" scope="row">עד לתאריך<\/th>.*?<\/td>/gms
-//     );
-//     dateString = dateString[0].replace(/<.*>\n/, '');
-//     dateString = dateString.replace(/<.*?>/gm, '');
-//     const date_time = dateString.split(', ');
-//     const date = date_time[0].split('/');
-//     const time = date_time[1].split(':');
-//     const date_1 = new Date(
-//       date[2],
-//       date[1],
-//       date[0],
-//       time[0],
-//       time[1]
-//     ).getTime();
-//     timeLeft.push(date_1);
-//   });
-//   return timeLeft;
-// }
-
-// async function countDown(timeString) {
-//   console.log('COUNT DOWN');
-//   await chrome.runtime.sendMessage(
-//     {
-//       type: 'timeString',
-//       timeString: timeString,
-//     },
-//     async (timeArray) => {
-//       await console.log('RESPONSE: ', timeArray);
-//     }
-//   );
-// }
-
-// countDown(timeStrings);
-
-// const xhr = new XMLHttpRequest();
-// xhr.open('GET', urlArray[0]);
-// xhr.send();
-// xhr.responseType = 'document';
-// xhr.onload = () => {
-//   if (xhr.status == 200) {
-//     const taskPage = xhr.response;
-//     const tableHtml = Array.from(taskPage.getElementsByTagName('tbody'));
-//     let dateString = String(tableHtml[0].innerHTML);
-//     dateString = dateString.match(
-//       /<th class="cell c0" style="" scope="row">עד לתאריך<\/th>.*?<\/td>/gms
-//     );
-//     dateString = dateString[0].replace(/<.*>\n/, '');
-//     dateString = dateString.replace(/<.*?>/gm, '');
-//     const date_time = dateString.split(', ');
-//     const date = date_time[0].split('/');
-//     const time = date_time[1].split(':');
-//     const timeToExpire = new Date(
-//       date[2],
-//       date[1],
-//       date[0],
-//       time[0],
-//       time[1]
-//     ).getTime();
-//   } else {
-//     console.log(`Error: ${xhr.status}`);
-//   }
-// };
-
-// tasks.forEach((task) => {
-//   urlArray.push(task.getElementsByTagName('a')[0]['href']);
-//   let spanText = task.getElementsByTagName('span')[0];
-//   // spanText.innerText = 'PUT NEW DATE';
-// });
-
-// const url_1 = fetch(urlArray[0])
-//   .then((response) => console.log(response))
-//   .catch((err) => console.log(err));
-
-// Communicate with background file by sending a message
-// chrome.runtime.sendMessage(
-//   {
-//     type: 'GREETINGS',
-//     payload: {
-//       message: 'Hello, my name is Con. I am from ContentScript.',
-//     },
-//   },
-//   (response) => {
-//     console.log(response);
-//   }
-// );
